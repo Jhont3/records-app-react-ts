858b3833c9bb1c35372839bb00180acdaeb04dfd
Jhonatan <jhont.escobar17@gmail.com>
2023-03-29 23:40:23 -0500

    basic structured lazy load nested by modules pending to ordenate to correct logic

--- reminder-app/src/App.tsx
+++ reminder-app/src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Navigation } from './routes/Navigation';
 
 function App() {
   return (
-   <><h1>Hola =3</h1></>
-  )
+    <>
+      <Navigation />
+    </>
+  );
 }
 
-export default App
+export default App;
